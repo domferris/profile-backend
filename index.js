557ef07dd9f9bf56005766159d5a70b1fdc61c50
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-22 17:51:46 -0500

    access github page html in console

--- index.js
+++ index.js
@@ -7,9 +7,26 @@ app.use(cors());
 
 app.get("/github_scrape", (req, res) => {
   console.log("github_scrape");
-  res.json({ text: "Hello World!" });
+  const data = scrapeGithub();
+  // res.json({ text: "Hello World!" });
+  res.json(data);
 });
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+// PUPPETEER
+const puppeteer = require("puppeteer");
+
+const scrapeGithub = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://github.com/domferris");
+
+  const content = await page.content();
+  console.log(content);
+
+  await browser.close();
+  return { content: content };
+};
