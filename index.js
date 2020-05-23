6b7bafb8586433c0a74ebe36f6002329c7d48d58
Dominic Ferris <ferris.dominic@gmail.com>
2020-05-23 16:01:27 -0500

    evaluate page and extract contributions number

--- index.js
+++ index.js
@@ -6,27 +6,42 @@ const port = 3000;
 app.use(cors());
 
 app.get("/github_scrape", (req, res) => {
-  console.log("github_scrape");
-  const data = scrapeGithub();
-  // res.json({ text: "Hello World!" });
+  console.log("github_scrape connected...");
+  const data = scrapeGithub("https://github.com/domferris");
+  console.log(data);
   res.json(data);
 });
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Profile backend listening at http://localhost:${port}`)
 );
 
 // PUPPETEER
 const puppeteer = require("puppeteer");
 
-const scrapeGithub = async () => {
+const scrapeGithub = async (profile) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://github.com/domferris");
+  await page.goto(profile);
 
-  const content = await page.content();
-  console.log(content);
+  const contributions = await page.$eval(
+    ".js-yearly-contributions h2",
+    (element) => {
+      return element.innerText.match(/\d+/)[0];
+    }
+  );
+
+  const data = {
+    contributions: 0,
+  };
+
+  const contributionsNum = parseInt(contributions, 10);
+
+  console.log(data);
+  data.contributions += contributionsNum;
+  console.log(data);
 
   await browser.close();
-  return { content: content };
+
+  return data;
 };
