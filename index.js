f47da4877d5f75b3b5ce1741fc192d252cfa4fa4
Dominic Ferris <ferris.dominic@gmail.com>
2020-06-02 15:19:39 -0500

    move scrapeGithub into separate file, create new file for contact form

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import scrapeGithub from "./scrape_github";
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,14 +7,11 @@ const port = 3000;
 
 app.use(cors());
 
-// RETRIEVES GITHUB CONTRIBUTIONS NUMBER
+// RETRIEVES GITHUB CONTRIBUTIONS NUMBER VIA PUPPETEER
 app.get("/scrape_github", async (req, res) => {
   console.log("scrape_github connected...");
 
   const data = await scrapeGithub("https://github.com/domferris");
-
-  // console.log("inside call => ");
-  // console.log(data);
 
   res.send(data);
 });
@@ -20,36 +19,3 @@ app.get("/scrape_github", async (req, res) => {
 app.listen(process.env.PORT || 3000, function () {
   console.log(`Profile backend listening at ${this.address().port}`);
 });
-
-////////////////// PUPPETEER //////////////////
-const puppeteer = require("puppeteer");
-
-const scrapeGithub = async (profile) => {
-  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  const page = await browser.newPage();
-  await page.goto(profile);
-
-  const contributions = await page.$eval(
-    ".js-yearly-contributions h2",
-    (element) => {
-      return element.innerText.match(/\d+/)[0];
-    }
-  );
-
-  const data = {
-    contributionsNum: 0,
-  };
-
-  const contributionsNum = parseInt(contributions, 10);
-
-  data.contributionsNum += contributionsNum;
-
-  // console.log("inside function => ");
-  // console.log(data);
-
-  await browser.close();
-
-  return data;
-};
-
-////////////////// CONTACT FORM //////////////////
